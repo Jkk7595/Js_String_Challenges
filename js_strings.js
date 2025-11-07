76c3f5b7f94d448ec62629aa865b1182182de0a6
Jkk7595 <jospeterkananu7595@gmail.com>
2025-11-07 09:15:41 +0300

    Replacing a Word

--- js_strings.js
+++ js_strings.js
@@ -1,3 +1,6 @@
+
+          // 1. Uppercase or Lowercase?
+
 // const str = prompt("Enter a string:");
 
 // // store the input/string entered
@@ -11,13 +14,19 @@
 //   console.log("Mixed");
 // }
 
+        //2. Check First Letter
+// function string () {
+//   const str = prompt("Enter a string:");
+//   if(str.startsWith ("A") || str.startsWith("a")){
+//     console.log("Starts with A")
+//   }else{
+//     console.log( "Does not start with A")
+//   }
+// }
+// string()
 
-function string () {
-  const str = prompt("Enter a string:");
-  if(str.startsWith ("A") || str.startsWith("a")){
-    console.log("Starts with A")
-  }else{
-    console.log( "Does not start with A")
-  }
-}
-string()
+          
+    // 3. Replace a Word
+     const input =  "I am sad today"
+     const output = input.replace("sad","happy")
+     console.log(output)
