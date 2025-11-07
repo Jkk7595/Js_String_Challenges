18becaf48af0059a1fbeb3d45860d0a79abc7d54
Jkk7595 <jospeterkananu7595@gmail.com>
2025-11-07 08:58:38 +0300

    Q2 Check First Letter

--- js_strings.js
+++ js_strings.js
@@ -1,12 +1,23 @@
-const str = prompt("Enter a string:");
+// const str = prompt("Enter a string:");
 
-// store the input/string entered
-const strEntered = str; 
+// // store the input/string entered
+// const strEntered = str; 
 
-if (strEntered === strEntered.toUpperCase()) {
-  console.log("UPPERCASE");
-} else if (strEntered === strEntered.toLowerCase()) {
-  console.log("LOWERCASE");
-} else {
-  console.log("Mixed");
+// if (strEntered === strEntered.toUpperCase()) {
+//   console.log("UPPERCASE");
+// } else if (strEntered === strEntered.toLowerCase()) {
+//   console.log("LOWERCASE");
+// } else {
+//   console.log("Mixed");
+// }
+
+
+function string () {
+  const str = prompt("Enter a string:");
+  if(str.startsWith ("A") || str.startsWith("a")){
+    console.log("Starts with A")
+  }else{
+    console.log( "Does not start with A")
+  }
 }
+string()
